f66cbb8bbd7f08902d50d5bb0a3dc8388518b4ec
Administrator <admin@phillips-mbp.attlocal.net>
2019-06-10 12:20:29 -0500

    Initial Commit

---
+++ marston_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  marston_sort
+//
+//  Created by Administrator on 6/10/19.
+//  Copyright © 2019 charles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
